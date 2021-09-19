3cb602301807649ca3d5f24ff4d039ed7aa1b8cf
LuK1337 <priv.luk@gmail.com>
2021-09-19 15:00:17 +0200

    fajita: Switch to hardware/oneplus components
    Change-Id: I65f981a66e45af969801e66fd72a9354a1fb2413

--- device.mk
+++ device.mk
@@ -35,13 +35,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita \
-    vendor.oneplus.fingerprint.extension@1.0 \
-    vendor.oneplus.hardware.display@1.0
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    device/oneplus/common
+    lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus
 
 # Inherit from oneplus sdm845-common
 $(call inherit-product, device/oneplus/sdm845-common/common.mk)
