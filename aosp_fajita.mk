9017ad42bd605bc8d8727d34252d7e83d86e5e3f
jhenrique09 <jhsv09@gmail.com>
2021-10-18 18:12:05 +0200

    fajita: Enable NGA
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- aosp_fajita.mk
+++ aosp_fajita.mk
@@ -28,6 +28,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_NAME := aosp_fajita
