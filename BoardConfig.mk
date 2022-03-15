a7241814957525538f2e6d1c929f26665d193548
Christopher N. Hesse <raymanfx@gmail.com>
2022-03-15 12:12:59 +0100

    fajita: Enforce common device & vendor tree presence
    A compiled build has no chance of booting without the common device setup
    and the prebuilt binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from oneplus sdm845-common
--include device/oneplus/sdm845-common/BoardConfigCommon.mk
+include device/oneplus/sdm845-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oneplus/fajita
 
@@ -33,4 +33,4 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # inherit from the proprietary version
--include vendor/oneplus/fajita/BoardConfigVendor.mk
+include vendor/oneplus/fajita/BoardConfigVendor.mk
